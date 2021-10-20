083f8845850b6c7695796d64e136983d7a94b49c
Aleksey Zhizhin <zhizhin309@yandex.ru>
2021-10-20 20:51:47 +0300

    ДЗ1(исправил 3 задачу)

--- java/src/Homework1/task3/Main.java
+++ java/src/Homework1/task3/Main.java
@@ -4,24 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] mass = new int[]{28, 5, 11, 1, 44, 17, 8};
+        int[] mass = new int[]{44, 5, 11, 1, 28, 17, 8};
         printMass(mass);
         int indexMax = 0;
-        int indexmin = 0;
+        int indexMin = 0;
+        int max=mass[indexMax];
+        int min=mass[indexMin];
         for (int i = 1; i < mass.length; i++) {
             if (mass[i] > mass[indexMax]) {
                 indexMax = i;
+                max=mass[i];
             }
-            if (mass[i] < mass[indexmin]) {
-                indexmin = i;
+            if (mass[i] < mass[indexMin]) {
+                indexMin = i;
+                min=mass[i];
             }
         }
-        int tmp = mass[0];
-        mass[0] = mass[indexmin];
-        mass[indexmin] = tmp;
-        tmp = mass[mass.length - 1];
-        mass[mass.length - 1] = mass[indexMax];
-        mass[indexMax] = tmp;
+        int tmp=mass[0];
+        mass[0]=min;
+        mass[indexMin]=tmp;
+        tmp=mass[mass.length-1];
+        mass[mass.length-1]=max;
+        mass[indexMin]=tmp;
         printMass(mass);
     }
 
