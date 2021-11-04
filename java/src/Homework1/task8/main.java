fd0c367eb752c3b76355225448048d0fb88a4d4e
alekseyZhizhin <zhizhin309@yandex.ru>
2021-11-04 19:55:41 +0300

    Задание 8

--- java/src/Homework1/task8/main.java
+++ java/src/Homework1/task8/main.java
@@ -1,7 +1,38 @@
 package Homework1.task8;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("1");
+        FileInputStream file = null;
+        int b;
+        int countWord = 0;
+        boolean k = true;
+        try {
+            file=new FileInputStream("/Users/alekseyzhizhin/Documents/GitHub/javaByCroc/java/src/Homework1/task8/input.txt");
+            //file = new FileInputStream(args[0]);
+            while ((b = file.read()) != -1) {
+                char symbol = (char) b;
+
+                if (symbol ==' '||symbol=='\n'){
+                    k=true;
+                }else {
+                    if (k==true){
+                        countWord++;
+                        k=false;
+                    }
+
+                }
+            }
+            System.out.println(countWord);
+        } catch (FileNotFoundException exception) {
+            System.out.println("Такого файла нет в каталоге");
+        } catch (IOException exception) {
+            System.out.println("Не удалось прочитать файл");
+
+        }
     }
 }
