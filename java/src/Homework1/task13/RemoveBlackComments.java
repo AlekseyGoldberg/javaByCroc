658667ad2bb197588e843e15270c7742295f1b79
alekseyZhizhin <zhizhin309@yandex.ru>
2021-11-17 14:34:09 +0300

    Сделал 13 задание

--- java/src/Homework1/task13/RemoveBlackComments.java
+++ java/src/Homework1/task13/RemoveBlackComments.java
@@ -6,7 +6,6 @@ import java.util.Set;
 public class RemoveBlackComments implements BlackListFilter {
     @Override
     public void filterComments(List<String> comments, Set<String> blackList) {
-        //придумать как плюсовать i потому что он удаляет строку и прибалвяет ожну забивая на одну строку
         for (int i = 0; i < comments.size();i++) {
             String[] mass = comments.get(i).split("");
             StringBuilder word = new StringBuilder();
@@ -16,7 +15,6 @@ public class RemoveBlackComments implements BlackListFilter {
                         mass[k].equals("\""))) {
                     word.append(mass[k]);
                 } else {
-//                    System.out.println(word);
                     if (blackList.contains(word.toString().toLowerCase())) {
                         comments.remove(i);
                         i--;
